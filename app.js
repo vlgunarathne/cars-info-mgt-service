79ff228b27a9a54d605ed7d208d0f7d949eb2ebd
Viduranga <vlgunarathne@gmail.com>
2024-12-02 09:10:51 +1100

    Add filter controller and format codebase

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var carsRouter = require('./routes/carRoutes');
+var filtersRouter = require('./routes/filterRoutes');
 
 var app = express();
 
@@ -16,14 +17,15 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/cars', carsRouter);
+app.use('/filters', filtersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- resources/dataMigration.js
+++ resources/dataMigration.js
@@ -6,34 +6,34 @@ const { carsCollection } = require('../config/firebase');
 const csvFilePath = path.join(__dirname, 'Automobile.csv');
 
 async function migrateData() {
-  const cars = [];
+    const cars = [];
 
-  fs.createReadStream(csvFilePath)
-    .pipe(csv())
-    .on('data', (row) => {
-      // Convert fields to appropriate types
-      row.mpg = parseFloat(row.mpg);
-      row.displacement = parseFloat(row.displacement);
-      row.acceleration = parseFloat(row.acceleration);
-      row.weight = parseFloat(row.weight);
-      row.cylinders = parseInt(row.cylinders, 10);
-      row.horsepower = parseInt(row.horsepower, 10);
-      row.model_year = parseInt(row.model_year, 10);
+    fs.createReadStream(csvFilePath)
+        .pipe(csv())
+        .on('data', (row) => {
+            // Convert fields to appropriate types
+            row.mpg = parseFloat(row.mpg);
+            row.displacement = parseFloat(row.displacement);
+            row.acceleration = parseFloat(row.acceleration);
+            row.weight = parseFloat(row.weight);
+            row.cylinders = parseInt(row.cylinders, 10);
+            row.horsepower = parseInt(row.horsepower, 10);
+            row.model_year = parseInt(row.model_year, 10);
 
-      cars.push(row);
-    })
-    .on('end', async () => {
-      console.log('CSV file successfully processed');
-      for (const car of cars) {
-        try {
-          await carsCollection.add(car);
-          console.log(`Car added: ${car.name}`);
-        } catch (error) {
-          console.error(`Error adding car: ${car.name}`, error);
-        }
-      }
-      console.log('Data migration completed.');
-    });
+            cars.push(row);
+        })
+        .on('end', async () => {
+            console.log('CSV file successfully processed');
+            for (const car of cars) {
+                try {
+                    await carsCollection.add(car);
+                    console.log(`Car added: ${car.name}`);
+                } catch (error) {
+                    console.error(`Error adding car: ${car.name}`, error);
+                }
+            }
+            console.log('Data migration completed.');
+        });
 }
 
 migrateData();
