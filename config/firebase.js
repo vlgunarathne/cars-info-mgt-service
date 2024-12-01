3fd1615d6884cc404bff94ff5f3d13ebc4931233
Viduranga <vlgunarathne@gmail.com>
2024-12-01 22:18:11 +1100

    Configure app default creds for k8s environments

--- config/firebase.js
+++ config/firebase.js
@@ -1,8 +1,10 @@
 const admin = require('firebase-admin');
 const serviceAccount = require('../resources/serviceAccountKey.json');
 
+const isAppDefaultCred = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'production';
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+    credential: isAppDefaultCred ? admin.credential.applicationDefault() : admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
