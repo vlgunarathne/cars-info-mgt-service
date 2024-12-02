b429c2e02e24448380a487f9b2b4e83b88e3e306
Viduranga <vlgunarathne@gmail.com>
2024-12-02 22:18:13 +1100

    Use app credentials

--- config/firebase.js
+++ config/firebase.js
@@ -1,18 +1,10 @@
 const admin = require('firebase-admin');
-const fs = require('fs');
-const path = require('path');
-
-const serviceAccountPath = path.join(__dirname, '../resources/serviceAccountKey.json');
-let serviceAccount;
-
-if (fs.existsSync(serviceAccountPath)) {
-    serviceAccount = require(serviceAccountPath);
-}
+// const serviceAccount = require('../resources/serviceAccountKey.json');
 
 const isAppDefaultCred = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'production';
 
 admin.initializeApp({
-    credential: !isAppDefaultCred && serviceAccount ? admin.credential.cert(serviceAccount): admin.credential.applicationDefault()
+    credential: admin.credential.applicationDefault() // admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
